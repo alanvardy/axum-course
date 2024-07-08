338966c58c2c8f9a3f1bc6505a8298cead05543c
Alan Vardy <alan@vardy.cc>
2024-07-08 07:09:04 -0700

    added basic route and test

---
+++ README.md
@@ -0,0 +1,17 @@
+# axum-course
+
+Re-run app on change
+
+```
+cargo watch -q -c -w src/ -x run 
+```
+Re-run tests on change
+
+```
+cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
+```
+
+https://www.youtube.com/watch?v=XZtlD_m59sM
+
+Current position:
+6:43
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    Html("Hello <strong>Alan!</strong>")
 }
